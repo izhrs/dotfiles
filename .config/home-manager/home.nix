fa6365f58880886be40eda3173df3b2bc5909d2a
izhrs <mohamed@izhar.xyz>
2025-05-17 23:38:03 +0530

    increased bg opacity and made padding uniform

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -65,9 +65,9 @@ in {
     font.size = 13;
     font.name = "JetBrainsMono Nerd Font Mono";
     settings = {
-      background_opacity = 0.3;
+      background_opacity = 0.9;
       placement_strategy = "center";
-      window_padding_width = "0 10";
+      window_padding_width = "10 10";
       background_blur = 40;
       hide_window_decorations = true;
       enable_audio_bell = false;
