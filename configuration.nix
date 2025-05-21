4038dcc6df064d919db19d56809885982ff4c1d0
izhrs <mohamed@izhar.xyz>
2025-05-22 00:41:27 +0530

    refactor: prefer nested configuration and add few more CLI and GUI packages as well as config to open few TCP ports using firewall

--- configuration.nix
+++ configuration.nix
@@ -3,23 +3,54 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  system.stateVersion = "24.11";
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Bootloader
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.timeout = 0;
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 3000 5173 8000 8080 ];
+      allowedUDPPorts = [ ];
+    };
+  };
 
-  # Set your time zone.
+  services = {
+    xserver.enable = true;
+    xserver.displayManager.gdm.enable = true;
+    xserver.desktopManager.gnome.enable = true;
+    xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+    # Enable touchpad support (enabled default in most desktopManager).
+    # xserver.libinput.enable = true;
+
+    printing.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+    # openssh.enable = true;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   time.timeZone = "Asia/Kolkata";
 
   # Select internationalisation properties.
@@ -37,64 +68,23 @@
     LC_TIME = "en_IN";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.izhrs = {
     isNormalUser = true;
     description = "Mohamed Izhar";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs;
-      [
-        #  thunderbird
-      ];
+    packages = [ ];
     shell = pkgs.nushell;
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
 
     # CLI
+    android-tools
     bat
     bottom
     cargo
@@ -115,6 +105,7 @@
     nodejs_24
     nodePackages.prettier # for nvim
     nushell
+    pciutils # for lspci and other commands
     procs
     python314
     ripgrep # for nvim
@@ -131,35 +122,15 @@
     zip
 
     # GUI
+    libreoffice
+    protonvpn-gui
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
-  # Enable Nix-ld for dynamic linking (running elf binaries)
-  programs.nix-ld.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  programs = {
+    firefox.enable = true;
+    # Enable Nix-ld for dynamic linking (running elf binaries)
+    nix-ld.enable = true;
+    steam.enable = true;
+  };
 }
