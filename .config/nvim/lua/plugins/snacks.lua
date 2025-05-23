4b8ecb318bce4442a1853eb84b2709556507820a
izhrs <mohamed@izhar.xyz>
2025-05-24 04:13:58 +0530

    refactor: prefer indent_width=4 and run format

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,19 +1,19 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    notifier = { enabled = true },
+    "folke/snacks.nvim",
+    opts = {
+        notifier = { enabled = true },
 
-    -- show hidden files in snacks.explorer
-    picker = {
-      sources = {
-        explorer = {
-          -- show hidden files like .env
-          hidden = true,
-          -- show files ignored by git like node_modules
-          ignored = true,
-          exclude = { "node_modules", ".git" },
+        -- show hidden files in snacks.explorer
+        picker = {
+            sources = {
+                explorer = {
+                    -- show hidden files like .env
+                    hidden = true,
+                    -- show files ignored by git like node_modules
+                    ignored = true,
+                    exclude = { "node_modules", ".git" },
+                },
+            },
         },
-      },
     },
-  },
 }
--- .config/nvim/lua/plugins/supertab.lua
+++ .config/nvim/lua/plugins/supertab.lua
@@ -1,24 +1,24 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = {
-      preset = "super-tab",
-    },
-    completion = {
-      list = {
-        selection = {
-          preselect = true,
-          auto_insert = false,
+    "saghen/blink.cmp",
+    opts = {
+        keymap = {
+            preset = "super-tab",
         },
-      },
+        completion = {
+            list = {
+                selection = {
+                    preselect = true,
+                    auto_insert = false,
+                },
+            },
+        },
+        enabled = function()
+            local disabled = false
+            disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
+            disabled = disabled or (vim.bo.buftype == "prompt")
+            disabled = disabled or (vim.fn.reg_recording() ~= "")
+            disabled = disabled or (vim.fn.reg_executing() ~= "")
+            return not disabled
+        end,
     },
-    enabled = function()
-      local disabled = false
-      disabled = disabled or (vim.tbl_contains({ "markdown" }, vim.bo.filetype))
-      disabled = disabled or (vim.bo.buftype == "prompt")
-      disabled = disabled or (vim.fn.reg_recording() ~= "")
-      disabled = disabled or (vim.fn.reg_executing() ~= "")
-      return not disabled
-    end,
-  },
 }
