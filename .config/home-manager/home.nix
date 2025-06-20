85f1d0ff958f85b293750b64de8c1062075082bc
izhrs <mohamed@izhar.xyz>
2025-06-21 01:13:03 +0530

    feat: add Wayland environment variables to home-manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -7,7 +7,18 @@
 
   home.file = { };
 
-  home.sessionVariables = { EDITOR = "nvim"; };
+  home.sessionVariables = {
+    EDITOR = "nvim";
+
+    NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+
+    XDG_SESSION_TYPE = "wayland";
+
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_QPA_PLATFORM = "wayland;xcb";
+  };
 
   # for non nixos
   # home.shell.enableNushellIntegration = true;
