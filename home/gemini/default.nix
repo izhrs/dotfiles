1dc092c82839130c47cc8120efa2c419ed95edb8
izhrs <mohamed@izhar.xyz>
2025-10-30 20:33:42 +0530

    gemini: fix the json schema

--- home/gemini/default.nix
+++ home/gemini/default.nix
@@ -1,13 +1,21 @@
 { pkgs, ... }: {
   home.file.".gemini/settings.json".text = builtins.toJSON {
-    hideTips = true;
-    hideBanner = true;
-    preferredEditor = "hx";
-    vimMode = true;
-    usageStatisticsEnabled = false;
-    theme = "Default";
-    selectedAuthType = "oauth-personal";
-    includeDirectories = [ "~/.gemini" ];
+    ui = {
+      hideTips = true;
+      hideBanner = true;
+      theme = "Default";
+    };
+
+    context = { includeDirectories = [ "~/.gemini" ]; };
+
+    general = {
+      preferredEditor = "hx";
+      vimMode = true;
+    };
+
+    security = { auth = { selectedType = "oauth-personal"; }; };
+
+    privacy = { usageStatisticsEnabled = false; };
   };
 
   home.packages = with pkgs; [ gemini-cli ];
