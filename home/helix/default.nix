d89c64b6c7a1e842551ce882956f271f3dcc9828
izhrs <mohamed@izhar.xyz>
2025-10-30 20:28:01 +0530

    helix(refactor): split keys and extraPackages into seperate modules
    Breaks down the monolithic helix configuration file into smaller, more
    manageable files. Keybindings are now in keys.nix and language
    server/tool packages are in extraPackages.nix. This improves readability
    and maintainability.

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -62,67 +62,7 @@
         };
       };
 
-      keys = {
-        normal = {
-          esc = [ "collapse_selection" "keep_primary_selection" ];
-          "{" = "goto_prev_paragraph";
-          "}" = "goto_next_paragraph";
-          H = "goto_previous_buffer";
-          L = "goto_next_buffer";
-
-          space = {
-            w = ":w";
-            q = ":q";
-            u = "switch_to_lowercase";
-            U = "switch_to_uppercase";
-
-            # yazi-picker script defined at bottom of this file
-            space = [
-              # using %% to escape 
-              ''
-                :sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker open %{buffer_name}''
-              ":redraw"
-            ];
-            l = [
-              ''
-                :sh zellij action new-pane --name "" --floating --width 80%% --height 80%% --x 10%% --y 10%% --close-on-exit -- lazygit''
-              ":redraw"
-            ];
-
-            t = {
-              s = ":toggle-option soft-wrap.enable";
-              u = "switch_case";
-            };
-          };
-
-          C-y = {
-            y = ''
-              :sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker open %{buffer_name}'';
-            # Open the file(s) in a vertical split
-            v = ''
-              :sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker vsplit %{buffer_name}'';
-            # Open the file(s) in a horizontal split
-            h = ''
-              :sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker hsplit %{buffer_name}'';
-          };
-        };
-
-        select = {
-          "{" = "goto_prev_paragraph";
-          "}" = "goto_next_paragraph";
-          space = {
-            u = "switch_to_lowercase";
-            U = "switch_to_uppercase";
-
-            t = {
-              s = ":toggle-option soft-wrap.enable";
-              u = "switch_case";
-            };
-          };
-        };
-
-        insert = { C-space = "completion"; };
-      };
+      keys = import ./keys.nix;
     };
 
     languages = import ./language.nix { inherit pkgs; };
@@ -136,56 +76,7 @@
       };
     };
 
-    extraPackages = with pkgs; [
-      lldb
-      clang-tools
-
-      # Toml
-      taplo
-
-      # Yaml
-      yaml-language-server
-
-      # Nix
-      nixd
-      nixfmt-classic
-
-      # Lua
-      stylua
-      lua-language-server
-
-      # Python
-      ruff
-      ty
-
-      # Bash / Shell
-      bash-language-server
-      shfmt
-
-      # Docker
-      dockerfile-language-server
-      dockfmt
-
-      # web
-      # vscode-css-language-server vscode-eslint-language-server vscode-html-language-server
-      # vscode-json-language-server vscode-markdown-language-server
-      vscode-langservers-extracted
-      emmet-ls
-
-      typescript-language-server
-      tailwindcss-language-server
-      svelte-language-server
-      prettier
-
-      # PostgreSQL
-      pgformatter
-
-      # Powershell
-      powershell-editor-services # powershell pkg in system/configuration.nix
-
-      # Markdown
-      markdown-oxide
-    ];
+    extraPackages = import ./extraPackages.nix { inherit pkgs; };
   };
 
   home.packages = with pkgs; [
