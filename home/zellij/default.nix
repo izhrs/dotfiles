8fd2a9c26abf4240317d06aaec01e956d5118813
izhrs <mohamed@izhar.xyz>
2025-11-14 16:19:48 +0530

    zellij(integration): auto update tab name based on current directory

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,4 +1,35 @@
-{ pkgs, inputs, ... }: {
+{ lib, pkgs, inputs, ... }: {
+
+  # lib.mkAfter means “append this Zsh configuration to the end of the specified
+  # config file”, which in this case is .zshrc.
+  # This config can be done in ../shell/default.nix too, but it's better here
+  # It's same as enableZshIntegration but custom
+  programs.zsh.initContent = lib.mkAfter ''
+    function zellij_tab_name_update() {
+      if [[ -n $ZELLIJ ]]; then
+        current_dir=$PWD
+
+        case "$current_dir" in
+          "$HOME")            current_dir=" " ;;
+          "$HOME/Downloads")  current_dir=" " ;;
+          "$HOME/Pictures")   current_dir=" " ;;
+          "$HOME/Videos")     current_dir=" " ;;
+          "$HOME/Documents")  current_dir=" " ;;
+          "$HOME/Documents/code")  current_dir=" " ;;
+          "$HOME/Documents/code/dotfiles")   current_dir="󱄅 " ;; # you would want to change this
+          *)
+            current_dir=''${current_dir##*/}
+            ;;
+        esac
+
+        command nohup zellij action rename-tab "$current_dir" >/dev/null 2>&1
+      fi
+    }
+
+    zellij_tab_name_update
+    autoload -Uz add-zsh-hook
+    add-zsh-hook chpwd zellij_tab_name_update'';
+
   programs.zellij = {
     enable = true;
     enableBashIntegration = true;
