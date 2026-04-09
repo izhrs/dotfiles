79a49e0129cd0437f390faddd549612c65d5ed37
izhrs <mohamed@izhar.xyz>
2026-04-09 23:40:58 +0530

    helix: better and cleaner integration

--- home/helix/keys.nix
+++ home/helix/keys.nix
@@ -20,20 +20,20 @@
       # DOES NOT WORK ANYMORE
       space = [
         # using %% to escape
-        '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker open %{buffer_name}''
+        '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% --close-on-exit --block-until-exit -- yazi-picker open %{buffer_name}''
         ":redraw"
       ];
 
       # Override default change picker
       g = [
-        '':sh zellij action new-pane --name "" --floating --width 80%% --height 80%% --x 10%% --y 10%% --close-on-exit -- lazygit''
+        '':sh zellij action new-pane --name "" --floating --width 80%% --height 80%% --x 10%% --y 10%% --close-on-exit --block-until-exit -- lazygit''
         ":redraw"
       ];
 
       # serpl is an intutive TUI find and replace tool
       # I don't know which key would make more sense here. using ";" because it's available and ergonomic
       ";" = [
-        '':sh zellij action new-pane --name "" --floating --width 80%% --height 80%% --x 10%% --y 10%% --close-on-exit -- serpl''
+        '':sh zellij action new-pane --name "" --floating --width 80%% --height 80%% --x 10%% --y 10%% --close-on-exit --block-until-exit -- serpl''
       ];
 
       # LLM integration
@@ -42,22 +42,22 @@
       l = {
         # chat
         c = [
-          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit -- gemini''
+          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit --block-until-exit -- gemini''
         ];
 
         # generate commit message
         m = [
-          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit -- llm-gen-commit-msg''
+          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit --block-until-exit -- llm-gen-commit-msg''
         ];
 
         # explain the codebase
         e = [
-          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit -- llm-explain''
+          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit --block-until-exit -- llm-explain''
         ];
 
         # analyze and suggest improvments
         a = [
-          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit -- llm-do-anal''
+          '':sh zellij action new-pane --name ""  --floating --width 35%% --height 96%% --x 70%% --y 2%% --close-on-exit --block-until-exit -- llm-do-anal''
         ];
       };
 
@@ -68,11 +68,11 @@
     };
 
     C-y = {
-      y = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker open %{buffer_name}'';
+      y = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% --close-on-exit --block-until-exit -- yazi-picker open %{buffer_name}'';
       # Open the file(s) in a vertical split
-      v = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker vsplit %{buffer_name}'';
+      v = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% --close-on-exit --block-until-exit -- yazi-picker vsplit %{buffer_name}'';
       # Open the file(s) in a horizontal split
-      h = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% -- yazi-picker hsplit %{buffer_name}'';
+      h = '':sh zellij run -n "" -c -f -x 10%% -y 10%% --width 80%% --height 80%% --close-on-exit --block-until-exit -- yazi-picker hsplit %{buffer_name}'';
     };
 
     m.w = "select_all";
