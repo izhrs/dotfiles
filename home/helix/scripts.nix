308f47fc401aeb68dcd93ec86fba4c96f3ca72cd
izhrs <mohamed@izhar.xyz>
2026-04-09 21:18:27 +0530

    fix(yazi-picker): fix the issue where yazi-picker won't open files in helix. the issue was caused by `--choser-file=/dev/stdout`

--- home/helix/scripts.nix
+++ home/helix/scripts.nix
@@ -1,14 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   home.packages = with pkgs; [
     # got this script from: https://yazi-rs.github.io/docs/tips/#helix-with-zellij
+    # fix applied from https://github.com/sxyazi/yazi/issues/3046
     (writeShellScriptBin "yazi-picker" ''
-      paths=$(yazi "$2" --chooser-file=/dev/stdout | while read -r; do printf "%q " "$REPLY"; done)
+      paths=$(echo $(yazi "$2" --chooser-file=/dev/stdout) | while read -r; do printf "%q " "$REPLY"; done)
 
       if [[ -n "$paths" ]]; then
       	zellij action toggle-floating-panes
       	zellij action write 27 # send <Escape> key
       	zellij action write-chars ":$1 $paths"
       	zellij action write 13 # send <Enter> key
+      	zellij action write 27 # send <Escape> key
       else
       	zellij action toggle-floating-panes
       fi
