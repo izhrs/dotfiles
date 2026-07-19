d8df7f0f6380f5fbcab850b6c0d5f4313a8a850b
izhrs <mohamed@izhar.xyz>
2026-07-19 15:13:42 +0530

    feat(theme): create theme switcher scripts and destop icon shortcut to use it in launchers

--- home/theme_switcher/default.nix
+++ home/theme_switcher/default.nix
@@ -1,16 +1,23 @@
 { pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
-    (writeShellScriptBin "switch-theme" ''
+    (writeShellScriptBin "light" ''
       $(fd -td ".*.home-manager-generation" /nix/store/ -x fd -td "specialisation" {} | head -1)/light/activate
+    '')
+
+    (writeShellScriptBin "dark" ''
+      $(fd -td ".*.home-manager-generation" /nix/store/ -x fd -td "specialisation" {} | head -1)/../activate
     '')
   ];
 
-  xdg.desktopEntries.theme_switcher = {
+  # I tried wrapping dark-mode desktop entry in light Specialisation so there would have been
+  # only one destop entry at a time but it didn't work. Same thing I did with scripts but no luck
+
+  xdg.desktopEntries.light_mode = {
     name = "Light Theme";
     genericName = "Light Theme";
     comment = "Switch to Light theme";
-    exec = "switch-theme";
+    exec = "light";
     terminal = false;
     type = "Application";
     icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/redshift.svg";
@@ -22,30 +29,19 @@
     startupNotify = true;
   };
 
-  specialisation.light.configuration = {
-    home.packages = lib.mkForce (
-      with pkgs;
-      [
-        (writeShellScriptBin "switch-theme" ''
-          $(fd -td ".*.home-manager-generation" /nix/store/ -x fd -td "specialisation" {} | head -1)/../activate
-        '')
-      ]
-    );
-
-    xdg.desktopEntries.theme_switcher = lib.mkForce {
-      name = "Dark Theme";
-      genericName = "Dark Theme";
-      comment = "Switch to Dark theme";
-      exec = "switch-theme";
-      terminal = false;
-      type = "Application";
-      icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/distributor-logo-budgie.svg";
-      categories = [
-        "Settings"
-        "DesktopSettings"
-        "Utility"
-      ];
-      startupNotify = true;
-    };
+  xdg.desktopEntries.dark_mode = lib.mkForce {
+    name = "Dark Theme";
+    genericName = "Dark Theme";
+    comment = "Switch to Dark theme";
+    exec = "dark";
+    terminal = false;
+    type = "Application";
+    icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/distributor-logo-budgie.svg";
+    categories = [
+      "Settings"
+      "DesktopSettings"
+      "Utility"
+    ];
+    startupNotify = true;
   };
 }
